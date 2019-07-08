ff5dc7d13c14bc00a3582cb4817febbf5d7b10b5
PAHJunior <pahjunior@outlook.com>
2019-07-08 17:03:58 -0300

    Testando hooks :alien:

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const { setUser } = require('../controller/usuarios.js')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Rota de usuarios');
 });
+
+
+/* GET users listing. */
+router.post('/', setUser);
 
 module.exports = router;
