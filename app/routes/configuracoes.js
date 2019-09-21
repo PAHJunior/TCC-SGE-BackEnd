ed04a0bc4867ddf9c3d80ebb7573f13af73c43c0
PAHJunior <pahjunior@outlook.com>
2019-09-21 12:47:32 -0300

    rota de configuracao :fire:

--- app/routes/configuracoes.js
+++ app/routes/configuracoes.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const { buscarConfiguracao } = require('../controller/configuracao_db')
+const { buscarConfiguracao, buscarUmaConfiguracao } = require('../controller/configuracao_db')
 
 /* GET usuarios listing. */
 router.get('/', buscarConfiguracao);
+
+router.get('/:database', buscarUmaConfiguracao);
+
 
 module.exports = router;
