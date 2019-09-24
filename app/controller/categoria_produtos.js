600e26e9ce8f3d3a63f6f162b7219f498355e56c
PAHJunior <pahjunior@outlook.com>
2019-09-23 23:44:09 -0300

    rota categoria_produtos - falta post e patch :fire:

--- app/controller/categoria_produtos.js
+++ app/controller/categoria_produtos.js
@@ -13,18 +13,43 @@ const {
       .then((categoria_produtos) => {
         if ((categoria_produtos == null) || (categoria_produtos == undefined) || (categoria_produtos.length == 0)) {
           res.status(404)
-            .send(util.response("Erro", 404, "Estoque não encontrado", "api/categoria_produtos", "GET", null))
+            .send(util.response("Erro", 404, "Categoria produto não encontrada", "api/categoria_produtos", "GET", null))
         } else {
           res.status(200)
-            .send(util.response("Buscar estoque", 200, categoria_produtos, "api/categoria_produtos", "GET", null))
+            .send(util.response("Buscar categoria produto", 200, categoria_produtos, "api/categoria_produtos", "GET", null))
         }
       }).catch((e) => {
         let error = console.error(e)
         res.status(400)
-          .send(util.response("Error", 400, 'Ocorreu um error ao buscar os estoques', "api/categoria_produtos", "GET", error))
+          .send(util.response("Error", 400, 'Ocorreu um error ao buscar a categoria do produto', "api/categoria_produtos", "GET", error))
+      })
+  }
+
+  const buscarUmaCategoria_produto = (req, res, next) => {
+    tbl_categoria_produtos.findAll({
+      attributes: {
+        exclude: ['createdAt', 'updatedAt']
+      },
+      where: {
+        id_categoria_produto: req.params.id
+      }
+    })
+      .then((categoria_produtos) => {
+        if ((categoria_produtos == null) || (categoria_produtos == undefined) || (categoria_produtos.length == 0)) {
+          res.status(404)
+            .send(util.response("Erro", 404, "Categoria produto não encontrada", "api/categoria_produtos", "GET", null))
+        } else {
+          res.status(200)
+            .send(util.response("Buscar categoria produto", 200, categoria_produtos, "api/categoria_produtos", "GET", null))
+        }
+      }).catch((e) => {
+        let error = console.error(e)
+        res.status(400)
+          .send(util.response("Error", 400, 'Ocorreu um error ao a categoria do produto', "api/categoria_produtos", "GET", error))
       })
   }
   
   module.exports = {
-    buscarCategoria_produtos
+    buscarCategoria_produtos,
+    buscarUmaCategoria_produto
   }
--- app/routes/categoria_produtos.js
+++ app/routes/categoria_produtos.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const { buscarCategoria_produtos } = require('../controller/categoria_produtos')
+const {
+    buscarCategoria_produtos,
+    buscarUmaCategoria_produto
+} = require('../controller/categoria_produtos')
 
-/* GET usuarios listing. */
+/* Buscar todas as categoria */
 router.get('/', buscarCategoria_produtos);
+
+/* Buscar uma categoria. */
+router.get('/:id', buscarUmaCategoria_produto);
 
 module.exports = router;
