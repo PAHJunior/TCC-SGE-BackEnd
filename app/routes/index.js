f423a3f0f5af0fafa1b7ea37055512124e16c5c0
PAHJunior <pahjunior@outlook.com>
2019-09-25 10:01:38 -0300

    production -teste :fire:

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sistema de gerenciamento de estoque' });
 });
 
 module.exports = router;
